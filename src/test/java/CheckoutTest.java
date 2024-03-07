8b692754e34ee79577334b2c5593d6c349a23f61
ItIsNotAUserName <tarkhov92@mail.ru>
2024-03-06 21:04:12 -0600

    commit

---
+++ src/test/java/CheckoutTest.java
@@ -0,0 +1,8 @@
+import org.junit.Test;
+
+public class CheckoutTest {
+    @Test
+    public void checkout(){
+        System.out.println("Checkout!!!");
+    }
+}
---
+++ src/test/java/LoginTest.java
@@ -0,0 +1,13 @@
+import org.junit.Test;
+
+public class LoginTest {
+    @Test
+    public void test(){
+        System.out.println("Test 1");
+    }
+
+    @Test
+    public void test2(){
+        System.out.println("Test 2");
+    }
+}
